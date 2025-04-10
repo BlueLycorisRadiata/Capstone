2cd1914203b09307c6f0edb9e01332d8e21d877e
BlueLycorisRadiata <nguyensyquanganh123@gmail.com>
2025-04-11 02:20:28 +0700

    SecurityV1

---
+++ src/main/java/fpt/sep/capstone/entity/Authority.java
@@ -0,0 +1,22 @@
+package fpt.sep.capstone.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "authority")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Authority implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "authority")
+    private String authority;
+}
